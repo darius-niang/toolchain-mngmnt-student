eafeb1231cd1b1dfbfada8addc37ea9f5336fd0d
unknown <darius-niang01@gmail.com>
2021-03-08 14:04:08 +0000

    ajout du dossier public

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel='stylesheet' type='text/css' href='css1/styles.css'>
+        <link rel='stylesheet' type='text/css' href='public/css1/styles.css'>
         <title>students 2</title>
     </head>
     
@@ -12,25 +12,25 @@
           <h1 class="card-title" align="center" style="width: 1000px ">Veuillez Choisir votre profil</h3><br><br>
           <h2>Etudiants</h1>
           <a href="register.php" class="btn btn-success">
-            <img src="images/2.jpg" alt="" width="200px">
+            <img src="public/images/2.jpg" alt="" width="200px">
           </a>
         </div>
         <div id="mes_photos"><br><br><br><br><br>
           <h2>Teachers Dashboad</h2>
           <a href="connprof.php" class="btn btn-primary">
-            <img src="images/3.png" alt="" width="200px">
+            <img src="public/images/3.png" alt="" width="200px">
           </a><br>
         </div>
         <div id="mes_amis"><br><br><br><br><br>
           <h2>Signature Etudiants</h2>
           <a href="signez.php" class="btn btn-primary">
-            <img src="images/images.jfif" alt="" width="200px">
+            <img src="public/images/images.jfif" alt="" width="200px">
           </a>
         </div>
 				<div id="mes_amis"><br><br><br><br><br>
           <h2>Student Dashboad</h2>
           <a href="student.php" class="btn btn-info">
-            <img src="images/computer.png" alt="" width="200px">
+            <img src="public/images/computer.png" alt="" width="200px">
           </a>
         </div>
       </footer>
