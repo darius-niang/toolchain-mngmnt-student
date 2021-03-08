267a87b51a60099688124cfa2db6c914312d588b
unknown <darius-niang01@gmail.com>
2021-03-08 13:02:14 +0000

    connect to db4free and memory application 128 to 64M

--- database.php
+++ database.php
@@ -1,8 +1,13 @@
 
-<?php
-$servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db="taken";
-    $conn = mysqli_connect($servername, $username, $password,$db);
+<?php 
+$servername = "db4free.net";
+$username = "fukyo_nd";
+$password = "niang@57";
+$db="monsiteweb";
+$conn = mysqli_connect($servername, $username, $password,$db);
+// $servername = "localhost";
+//     $username = "root";
+//     $password = "";
+//     $db="taken";
+//     $conn = mysqli_connect($servername, $username, $password,$db);
     ?>
